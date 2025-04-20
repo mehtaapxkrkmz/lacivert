abbe42bb73878eb047c97f4896dbade1408aa0a9
NidaEkinci <nidaekinci2032@gmail.com>
2025-04-21 00:29:05 +0300

    logo, layouta eklendi

--- Frontend/src/components/guest/Layout/Header.jsx
+++ Frontend/src/components/guest/Layout/Header.jsx
@@ -7,7 +7,7 @@ import { CiLogin } from "react-icons/ci";
 import { RiMenu3Line } from "react-icons/ri";
 import { NavLink } from 'react-router-dom';
 import CategoryNavMenu from './CategoryNavMenu';
-import logo from '../../../assets/lacivert-logo.png';
+import logo from './lacivert-logo.png';
 
 const Header = ({ cartItemCount }) => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
