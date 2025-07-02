982a177fbafe64591f520dd84d47c6ffb16c998c
Sude Melek Acar <sudeacar86@gmail.com>
2025-07-02 16:02:21 +0300

    auth.js düzenlendi

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -143,7 +143,7 @@ const transporter = nodemailer.createTransport({
 });
 
 
-  const resetUrl = `https://lacivert-proje2.vercel.app/reset-password/${token}`;
+  const resetUrl = `http://lacivert.com/reset-password/${token}`;
   await transporter.sendMail({
     to: user.email,
     subject: 'Şifre Sıfırlama',
